cc44e3248d87c241a1ee376466f4350ac69a9a8d
Júlio Vaz <julio.oliveiravaz0@gmail.com>
2020-05-18 16:02:33 -0300

    test using action nodejs and self-runner

--- public/views/RoomView.js
+++ public/views/RoomView.js
@@ -1,14 +1,15 @@
-import View from '../views/View.js';
+import View from "../views/View.js";
 class RoomView extends View {
-    constructor(element){
-        super(element);
-    }
+  constructor(element) {
+    super(element);
+  }
 
-    generateHTML = (model) => `
+  generateHTML = (model) => `
         <div class="rooms-name">
             <h1>${model.room_name}</h1>
             <div>
                 <h2>${model.user_nickname}</h2>
+                <h3>github-actions</h3>
             </div> 
         </div>
     `;
